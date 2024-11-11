4af3676f7f65acb36deeeeb465efa58cccec60fb
Wilson.H <wilson_w_huang@dimerco.com>
2024-11-11 18:27:37 +0800

    feat: save

--- src/utils/oidcConfig.ts
+++ src/utils/oidcConfig.ts
@@ -4,19 +4,30 @@ import {
   WebStorageStateStore
 } from "oidc-client-ts";
 
+// const oidcSettings: UserManagerSettings = {
+//   authority: "https://ssotest.dimerco.com:8399",
+//   client_id: "esam3.0",
+//   redirect_uri: "https://10.161.252.171:5501/#/callback",
+//   post_logout_redirect_uri: "https://10.161.252.171:5501/#/",
+//   response_type: "code",
+//   scope: "openid profile api.dc.read api.dc.write dimercoinfo offline_access",
+//   userStore: new WebStorageStateStore({ store: window.localStorage }),
+//   stateStore: new WebStorageStateStore({ store: window.localStorage }),
+//   automaticSilentRenew: true,
+//   silent_redirect_uri: "https://10.161.252.171:5501/#/silent-renew"
+// };
 const oidcSettings: UserManagerSettings = {
-  authority: "https://ssotest.dimerco.com:8399",
-  client_id: "esam3.0",
-  redirect_uri: "https://10.161.252.171:5501/#/callback",
-  post_logout_redirect_uri: "https://10.161.252.171:5501/#/",
+  authority: import.meta.env.VITE_OIDC_AUTHORITY,
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI,
+  post_logout_redirect_uri: import.meta.env.VITE_OIDC_LOGOUT_URI,
   response_type: "code",
   scope: "openid profile api.dc.read api.dc.write dimercoinfo offline_access",
   userStore: new WebStorageStateStore({ store: window.localStorage }),
   stateStore: new WebStorageStateStore({ store: window.localStorage }),
   automaticSilentRenew: true,
-  silent_redirect_uri: "https://10.161.252.171:5501/#/silent-renew"
+  silent_redirect_uri: import.meta.env.VITE_OIDC_SILENT_REDIRECT_URI
 };
-
 const userManager = new UserManager(oidcSettings);
 
 export default userManager;
