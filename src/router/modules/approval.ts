0d7d4c8b2e21b1ce62578a93eff54af59653c25b
Wilson <25114872+Wil500000n@users.noreply.github.com>
2024-12-06 17:08:43 +0800

    feat: ams setting ui adjust

---
+++ src/router/modules/approval.ts
@@ -0,0 +1,22 @@
+import { $t } from "@/plugins/i18n";
+import { deal } from "@/router/enums";
+
+export default {
+  path: "/approval",
+  redirect: "/approval/index",
+  meta: {
+    icon: "ri:draft-line",
+    title: $t("approval.list"),
+    rank: deal
+  },
+  children: [
+    {
+      path: "/approval/index",
+      name: "ApprovalList",
+      component: () => import("@/views/approval/index.vue"),
+      meta: {
+        title: $t("approval.list")
+      }
+    }
+  ]
+} satisfies RouteConfigsTable;
---
+++ src/views/approval/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// import { useI18n } from "vue-i18n";
+// const { t } = useI18n();
+// import { ref, reactive, onMounted, computed, nextTick } from "vue";
+// import { useRenderIcon } from "@/components/ReIcon/src/hooks";
+// import CustomerQuickFilterService from "@/services/commonService";
+// import { useTourStoreHook } from "@/store/modules/tour";
+// import { useRouter } from "vue-router";
+defineOptions({
+  name: "ApprovalList"
+});
+</script>
+
+<template>
+  <div>25625</div>
+</template>
