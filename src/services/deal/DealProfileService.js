21715272830c411fbd990dc0189e7d009d8db5a9
Andy Kang <andy_kang@dimerco.com>
2025-01-07 16:46:13 +0800

    feat(deal APP-5372): deal UI update

--- src/services/deal/DealProfileService.js
+++ src/services/deal/DealProfileService.js
@@ -11,6 +11,65 @@ class DealProfileService {
       return [];
     }
   }
+  async getDealDetailsResult(DealID) {
+    try {
+      const url = `/api/Deal/DealDetailsResult?DealID=${DealID}`;
+      const response = await API.get(url);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async getDealTodolistResult(DealID) {
+    try {
+      const url = `/api/Deal/DealTodolistResult?DealID=${DealID}`;
+      const response = await API.get(url);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async getLinkedTaskDataList(params) {
+    try {
+      const response = await API.post(
+        "/api/Deal/GetLinkedTaskDataList",
+        params
+      );
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async getLinkedContactDataList(params) {
+    try {
+      const response = await API.post(
+        "/api/Deal/GetLinkedContactDataList",
+        params
+      );
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async getLinkedQuoteDataList(params) {
+    try {
+      const response = await API.post(
+        "/api/Deal/GetLinkedQuoteDataList",
+        params
+      );
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async updateLinkData(params) {
+    try {
+      const response = await API.post("/api/Deal/UpdateLinkData", params);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 export default new DealProfileService();
